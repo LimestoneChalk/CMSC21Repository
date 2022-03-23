9650dad8e9fca6c2acddda8fc448c9f02e9bfe41
Bonifer Gotas <bonigotas@gmail.com>
2022-03-23 04:27:42 -0400

    added Lecture 3 Activities

---
+++ Lecture3/Assignments/as1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main (void)
+{
+    int age;
+    bool teenager;
+    teenager = (age >= 13? (age<=19?true:false): false);
+    printf("%d", teenager);
+}
